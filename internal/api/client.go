89f5ef2d25b1ea01f004b12fa1b72a94945aff88
sarah-farag <sarahfarag125@gmail.com>
2025-05-07 08:01:19 +0100

    api client

--- internal/api/client.go
+++ internal/api/client.go
@@ -1,5 +1,23 @@
 package api
 
+import (
+	"net/http"
+	"time"
+)
+
 const (
 	WeatherAPI = "https://api.open-meteo.com/v1/forecast"
+	GeoAPI     = "https://geocoding-api.open-meteo.com/v1/search"
 )
+
+type APIClient struct {
+	Client *http.Client
+}
+
+func NewAPIClient() *APIClient {
+	return &APIClient{
+		Client: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
