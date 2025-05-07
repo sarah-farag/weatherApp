6d2a357a31cb29ce385339eae777a5f9d876bdda
sarah-farag <sarahfarag125@gmail.com>
2025-05-07 08:09:07 +0100

    decode respons

--- internal/api/client.go
+++ internal/api/client.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -20,4 +22,18 @@ func NewAPIClient() *APIClient {
 			Timeout: 10 * time.Second,
 		},
 	}
+}
+
+func (c *APIClient) GetWeather(lat, lon float64) (*models.WeatherResponse, error) {
+	url := fmt.Sprintf("%s?latitude=%.4f&longitude=%.4f&hourly=temperature_2m", WeatherAPI, lat, lon)
+
+	resp, err := c.Client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result models.WeatherResponse
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	return &result, err
 }
